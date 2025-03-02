3d88a4bc45f15aff047f36b25cca66e3f65a8bbe
Chris Wong <chris.wong.ksl@gmail.com>
2025-03-02 21:33:35 +0800

    Route Transitions

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
     <RouterView v-slot="{ Component }">
-      <Transition name="page" mode="out-in">
+      <transition name="page" mode="out-in">
         <component :is="Component" />
-      </Transition>
+      </transition>
     </RouterView>
   </div>
 </template>
@@ -21,7 +21,7 @@ import SiteNavigation from './components/SiteNavigation.vue'
 }
 
 .page-enter-from,
-.page-enter-to {
+.page-leave-to {
   opacity: 0;
 }
 </style>
